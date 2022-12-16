27c737fd4f15947a78a2a11db79bd1bedba8167d
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2022-12-16 18:36:55 -0300

    Tests and use cases

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a Notification content', () => {
-  const content = new Content('Você recebeu uma solicitação de amizade');
+describe('Notification content', () => {
+  it(' should be able to create a Notification content', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('It should not be able to create a Notification content with less than 5 characters', () => {
-  expect(() => new Content('Oi')).toThrow();
-});
+  it(' should not be able to create a Notification content with less than 5 characters', () => {
+    expect(() => new Content('Oi')).toThrow();
+  });
 
-test('It should not be able to create a Notification content with more than 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it(' should not be able to create a Notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
